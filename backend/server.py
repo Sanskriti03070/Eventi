95f21b0b82a79c974d131179d0acece8df09b3d9
Sanskriti <sankovss085@gmail.com>
2026-04-02 01:18:31 +0000

    final port fix

--- backend/server.py
+++ backend/server.py
@@ -581,3 +581,9 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+
+    import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
